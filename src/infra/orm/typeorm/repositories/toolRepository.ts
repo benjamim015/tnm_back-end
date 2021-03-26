3065408e186af4a21fbb3d8d89514ac76e988702
Benjamim <benjamimq015@gmail.com>
2021-03-25 22:14:55 -0400

    feat: add loadAll in ToolTypeORMRepository

--- src/infra/orm/typeorm/repositories/toolRepository.ts
+++ src/infra/orm/typeorm/repositories/toolRepository.ts
@@ -1,14 +1,21 @@
 import { AddToolRepository } from '../../../../data/protocols/addToolRepository';
+import { LoadToolsRepository } from '../../../../data/protocols/loadToolsRepository';
 import { ToolModel } from '../../../../domain/models/tool';
 import { AddToolParams } from '../../../../domain/useCases/addTool';
 import { Tool } from '../entities/tool';
 import { TypeORMHelper } from '../helper';
 
-export class ToolTypeORMRepository implements AddToolRepository {
+export class ToolTypeORMRepository implements AddToolRepository, LoadToolsRepository {
   async add(toolData: AddToolParams): Promise<ToolModel> {
     const toolRepository = TypeORMHelper.instance.getRepository(Tool);
     const tool = toolRepository.create(toolData);
     await toolRepository.save(tool);
     return tool;
+  }
+
+  async loadAll(): Promise<ToolModel[]> {
+    const toolRepository = TypeORMHelper.instance.getRepository(Tool);
+    const tools = await toolRepository.find();
+    return tools;
   }
 }
