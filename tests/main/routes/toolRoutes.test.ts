46d23e567addfaf752026c63d22439632baaade2
Benjamim <benjamimq015@gmail.com>
2021-03-25 22:34:10 -0400

    test: add LoadToolsRoute

--- tests/main/routes/toolRoutes.test.ts
+++ tests/main/routes/toolRoutes.test.ts
@@ -15,12 +15,20 @@ describe('CarShop Routes', () => {
     await TypeORMHelper.instance.disconnect();
   });
 
-  it('should return an tool on success', async () => {
-    await request(app).post('/api/tools').send({
-      title: 'any_title',
-      link: 'any_link',
-      description: 'any_description',
-      tags: ['any_tag'],
-    }).expect(201);
+  describe('POST /tools', () => {
+    it('should return an tool on success', async () => {
+      await request(app).post('/api/tools').send({
+        title: 'any_title',
+        link: 'any_link',
+        description: 'any_description',
+        tags: ['any_tag'],
+      }).expect(201);
+    });
+  });
+
+  describe('GET /tools', () => {
+    it('should return all tools on success', async () => {
+      await request(app).get('/api/tools').expect(200);
+    });
   });
 });
