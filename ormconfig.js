f4c338630902b2f81b8f7096a8ccf48b5d0d9355
Benjamim <benjamimq015@gmail.com>
2021-03-27 13:51:27 -0400

    chore: change ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,24 +1,30 @@
 const dir = process.env.NODE_ENV === 'prod' ? 'dist' : 'src';
 const extension = process.env.NODE_ENV === 'prod' ? 'js' : 'ts';
 
-console.log("NODE_ENV", process.env.NODE_ENV);
-console.log("PG_HOST", process.env.PG_HOST);
-console.log("PG_USERNAME", process.env.PG_USERNAME);
-console.log("PG_PASSWORD", process.env.PG_PASSWORD);
-console.log("PG_DATABASE", process.env.PG_DATABASE);
-console.log("PG_PORT", process.env.PG_PORT);
-console.log("API_PORT", process.env.API_PORT);
+console.log("DATABASE_URL > ", process.env.DATABASE_URL);
 
-module.exports = {
-  type: 'postgres',
-  host: process.env.NODE_ENV === 'prod' ? process.env.PG_HOST : 'localhost',
-  port: process.env.PG_PORT,
-  username: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
-  database: process.env.NODE_ENV === 'prod' ? process.env.PG_DATABASE : 'tnm_tests',
-  entities: [`./${dir}/infra/orm/typeorm/entities/*.${extension}`],
-  migrations: [`./${dir}/infra/orm/typeorm/migrations/*.${extension}`],
-  cli: {
-    migrationsDir: `./${dir}/infra/orm/typeorm/migrations`
+if (process.env.DATABASE_URL) {
+  module.exports = {
+    type: 'postgres',
+    url: 'postgres://easyevmipblpzf:c28c083388620ab4ba57380e8a89d17a7e14dfbc5ff8c73f3b1c9775751b4dd1@ec2-54-145-102-149.compute-1.amazonaws.com:5432/d3tk04ks91b85r',
+    entities: [`./${dir}/infra/orm/typeorm/entities/*.${extension}`],
+    migrations: [`./${dir}/infra/orm/typeorm/migrations/*.${extension}`],
+    cli: {
+      migrationsDir: `./${dir}/infra/orm/typeorm/migrations`
+    }
   }
+} else {
+  module.exports = {
+    type: 'postgres',
+    host: process.env.NODE_ENV === 'prod' ? 'postgres' : 'localhost',
+    port: process.env.PG_PORT,
+    username: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    database: process.env.NODE_ENV === 'prod' ? 'tnm' : 'tnm_tests',
+    entities: [`./${dir}/infra/orm/typeorm/entities/*.${extension}`],
+    migrations: [`./${dir}/infra/orm/typeorm/migrations/*.${extension}`],
+    cli: {
+      migrationsDir: `./${dir}/infra/orm/typeorm/migrations`
+    }
+  }  
 }
