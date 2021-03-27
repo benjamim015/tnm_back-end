536f11fb77024230ea117e001b0a0f57a7f1c286
Benjamim <benjamimq015@gmail.com>
2021-03-27 13:06:20 -0400

    chore: change start script and ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -3,11 +3,11 @@ const extension = process.env.NODE_ENV === 'prod' ? 'js' : 'ts';
 
 module.exports = {
   type: 'postgres',
-  host: process.env.NODE_ENV === 'prod' ? 'postgres' : 'localhost',
+  host: process.env.PG_HOST || process.env.NODE_ENV === 'prod' ? 'postgres' : 'localhost',
   port: process.env.PG_PORT,
   username: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
-  database: process.env.NODE_ENV === 'prod' ? 'tnm' : 'tnm_tests',
+  database: process.env.PG_DATABASE || process.env.NODE_ENV === 'prod' ? 'tnm' : 'tnm_tests',
   entities: [`./${dir}/infra/orm/typeorm/entities/*.${extension}`],
   migrations: [`./${dir}/infra/orm/typeorm/migrations/*.${extension}`],
   cli: {
